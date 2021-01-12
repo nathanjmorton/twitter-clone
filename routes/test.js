848a5a60f90704cefb9a965161b9511e98109245
Nathan Morton <nathan.j.morton@gmail.com>
2021-01-12 04:24:52 -0500

    this

--- routes/test.js
+++ routes/test.js
@@ -5,31 +5,32 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
 const User = require('../schemas/UserSchema');
 
-app.use(bodyParser.urlencoded({ extended: false }));
-
 app.set('view engine', 'pug');
 app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 router.get('/', (req, res, next) => {
   res.status(200).render('login');
 });
 
 router.post('/', async (req, res, next) => {
-  let payload = req.body;
+  var payload = req.body;
 
   if (req.body.logUsername && req.body.logPassword) {
-    let user = await User.findOne({
+    var user = await User.findOne({
       $or: [
         { username: req.body.logUsername },
         { email: req.body.logUsername },
       ],
-    }).catch((err) => {
-      console.log(err);
-      payload.errorMessage = 'Something went wrong';
+    }).catch((error) => {
+      console.log(error);
+      payload.errorMessage = 'Something went wrong.';
       res.status(200).render('login', payload);
     });
-    if (user !== null) {
-      let result = await bcrypt.compare(req.body.logPassword, user.password);
+
+    if (user != null) {
+      var result = await bcrypt.compare(req.body.logPassword, user.password);
 
       if (result === true) {
         req.session.user = user;
@@ -37,10 +38,11 @@ router.post('/', async (req, res, next) => {
       }
     }
 
-    payload.errorMessage = 'Login credentials incorrect';
+    payload.errorMessage = 'Login credentials incorrect.';
     return res.status(200).render('login', payload);
   }
-  payload.errorMessage = 'Make sure each field has a valid value';
+
+  payload.errorMessage = 'Make sure each field has a valid value.';
   res.status(200).render('login');
 });
 
