61c9108081b5d73886fd9bc863d2072a73eed58b
Nathan Morton <nathan.j.morton@gmail.com>
2021-01-12 21:29:47 -0500

    this

--- public/js/common.js
+++ public/js/common.js
@@ -29,5 +29,44 @@ $('#submitPostButton').click((e) => {
 });
 
 const createPostHtml = (postData) => {
-  return postData.content;
+  const {
+    postedBy: { profilePic, username, firstName, lastName, createdAt },
+    content,
+  } = postData;
+  const displayName = firstName + ' ' + lastName;
+
+  return `<div class='post'>
+    <div class='mainContentContainer'>
+      <div class='userImageContainer'>
+        <img src='${profilePic}'>
+      </div>
+      <div class='postContentContainer'>
+        <div class='header'>
+          <a href='/profile/${username}' class='displayName'>${displayName}</a>
+          <span class='username'>@${username}</span>
+          <span class='date'>${createdAt}</span>
+        </div>
+        <div class='postBody'>
+          <span>${content}</span>
+        </div>
+        <div class='postFooter'>
+          <div class='postButtonContainer'>
+            <button>
+              <i class='far fa-comment'></i>
+            </button>
+          </div>
+          <div class='postButtonContainer'>
+            <button>
+              <i class='fas fa-retweet'></i>
+            </button>
+          </div>
+          <div class='postButtonContainer'>
+            <button>
+              <i class='far fa-heart'></i>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>`;
 };
