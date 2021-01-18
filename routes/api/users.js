d8006a8cf50993a799df2a8c1ce1c505bf279a4f
Nathan Morton <nathan.j.morton@gmail.com>
2021-01-17 22:39:33 -0500

    this

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,7 +8,18 @@ const Post = require('../../schemas/PostSchema');
 app.use(bodyParser.urlencoded({ extended: false }));
 
 router.put('/:userId/follow', async (req, res, next) => {
-  res.status(200).send('foobar');
+  const userId = req.params.userId;
+
+  const user = await User.findById(userId);
+
+  if (user === null) {
+    return res.sendStatus(404);
+  }
+
+  const isFollowing =
+    user.followers && user.followers.includes(req.session.user._id);
+
+  res.status(200).send(isFollowing);
 });
 
 module.exports = router;
