b3bf45996eed8244eb3b75774900b6d19d5c079f
Nathan Morton <nathan.j.morton@gmail.com>
2021-01-18 19:50:20 -0500

    this

--- public/js/followersAndFollowing.js
+++ public/js/followersAndFollowing.js
@@ -8,16 +8,37 @@ $(document).ready(() => {
 
 const loadFollowers = () => {
   $.get(`/api/users/${profileUserId}/followers`, (results) => {
-    outputUsers(results, $('.resultsContainer'));
+    outputUsers(results.followers, $('.resultsContainer'));
   });
 };
 
 const loadFollowing = () => {
   $.get(`/api/users/${profileUserId}/following`, (results) => {
-    outputUsers(results, $('.resultsContainer'));
+    outputUsers(results.following, $('.resultsContainer'));
   });
 };
 
-const outputUsers = (data, container) => {
-  console.log(data);
+const outputUsers = (results, container) => {
+  container.html('');
+
+  results.forEach((result) => {
+    html = createUserHtml(result, true);
+    container.append(html);
+  });
+};
+
+const createUserHtml = (userData, showFollowButton) => {
+  const name = `${userData.firstName} ${userData.lastName}`;
+
+  return `<div class='user'>
+    <div class='userImageContainer'>
+      <img src='${userData.profilePic}' />
+    </div>
+    <div class='userDetailsContainer'>
+      <div class='header'>
+        <a href='/profile/${userData.username}'>${name}</a>
+        <span class='username'>@${userData.username}</span>
+      </div>
+    </div>
+  </div>`;
 };
